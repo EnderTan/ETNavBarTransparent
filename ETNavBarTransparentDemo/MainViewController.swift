4f0922bb35266ca4df23cba66643305125bd7c29
EnderTan <bing@BingdeMacBook-Pro.local>
2017-03-07 16:57:25 +0800

    Initial commit

---
+++ ETNavBarTransparentDemo/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  ETNavBarTransparentDemo
+//
+//  Created by Bing on 2017/3/1.
+//  Copyright © 2017年 tanyunbing. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ ETNavBarTransparentDemo/ProfileViewController.swift
@@ -0,0 +1,101 @@
+//
+//  ProfileViewController.swift
+//  ETNavBarTransparentDemo
+//
+//  Created by Bing on 2017/3/1.
+//  Copyright © 2017年 tanyunbing. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    fileprivate var statusBarShouldLight = true
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        automaticallyAdjustsScrollViewInsets = false
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        self.navBarBgAlpha = 0
+        self.navBarTintColor = .white
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        if statusBarShouldLight {
+            return .lightContent
+        } else {
+            return .default
+        }
+    }
+
+    
+}
+
+extension ProfileViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "profileCell", for: indexPath)
+        cell.textLabel?.text = "第\(indexPath.row)行"
+        return cell
+    }
+    
+}
+
+extension ProfileViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    //MARK:UIScrollViewDelegate
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let contentOffsetY = scrollView.contentOffset.y
+        let showNavBarOffsetY = 200 - topLayoutGuide.length
+        
+        
+        //navigationBar alpha
+        if contentOffsetY > showNavBarOffsetY  {
+            var navAlpha = (contentOffsetY - (showNavBarOffsetY)) / 40.0
+            if navAlpha > 1 {
+                navAlpha = 1
+            }
+            navBarBgAlpha = navAlpha
+            if navAlpha > 0.8 {
+                navBarTintColor = UIColor.defaultNavBarTintColor()
+                statusBarShouldLight = false
+                
+            }else{
+                navBarTintColor = UIColor.white
+                statusBarShouldLight = true
+            }
+        }else{
+            navBarBgAlpha = 0
+            navBarTintColor = UIColor.white
+            statusBarShouldLight = true
+        }
+        setNeedsStatusBarAppearanceUpdate()
+        
+        
+        
+        
+    }
+    
+    
+    
+}
